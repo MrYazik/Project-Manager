e68f8b412ed381e8053892a6e783dc21e07636ee
MrYaz <LehaHotPlay@yandex.ru>
2026-05-17 21:11:22 +0500

    Добавленна утилита для получения имени системы (util/getOSName.java)

--- app/src/main/java/mryazik/github/io/util/getOSName.java
+++ app/src/main/java/mryazik/github/io/util/getOSName.java
@@ -1,4 +1,15 @@
 package mryazik.github.io.util;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class getOSName {
+    static Logger logger = Logger.getLogger(getOSName.class.getName());
+
+    public static String get()
+    {
+        String osName = System.getProperty("os.name").toLowerCase();
+        logger.log(Level.INFO, "os_name: " + osName); // win/mac/nix
+        return osName;
+    }
 }
