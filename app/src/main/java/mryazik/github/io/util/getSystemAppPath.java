e42d1920d8a34bdf989ea903e1569ce4a40a98c4
MrYaz <LehaHotPlay@yandex.ru>
2026-05-17 21:12:26 +0500

    Добавленна утилита для получения системного пути (util/getSystemAppPath.java)

---
+++ app/src/main/java/mryazik/github/io/util/getSystemAppPath.java
@@ -0,0 +1,31 @@
+package mryazik.github.io.util;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class getSystemAppPath {
+    private static String appName = "ProjectManager";
+    private static Logger logger = Logger.getLogger(getSystemAppPath.class.getName());
+
+    public static Path get()
+    {
+        String systemName = getOSName.get();
+
+        if (systemName.contains("mac")) {
+            String userHome = System.getProperty("user.home") + "/Applications/" + appName;
+            return Paths.get(userHome);
+        } else if (systemName.contains("win")) {
+            String userHome = System.getProperty("user.home") + "/AppData/" + appName;
+            return Paths.get(userHome);
+        } else if (systemName.contains("nix"))
+        {
+            String userHome = "/var/lib/" + appName;
+            return Paths.get(userHome);
+        } else {
+            logger.log(Level.SEVERE, "Не удалось определить имя системы. systemName: " + systemName);
+            return null;
+        }
+    }
+}
